799f6650814f96371805e9359108bf7f24070f9c
gzoanetti <german.zoanetti@gmail.com>
2025-03-02 21:12:09 +0100

    HTMLNode class + tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 def main():
     new_object = TextNode("Test text", TextType.BOLD, "http://www.example.com")
+    print(new_object)
+    new_object = HTMLNode("p", "A test paragraph", "a", {"href": "https://www.google.com"})
     print(new_object)
 
 if __name__ == "__main__":
